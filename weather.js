251dcd40e06d0680d598d2f09dc3921935b752d0
Aakash Khamaru <130301578+iambatmanscape@users.noreply.github.com>
2023-06-25 20:39:43 +0530

    Update weather.js
    shows hourly data instead of daily data

--- weather.js
+++ weather.js
@@ -8,8 +8,8 @@ if(fil.val==='') {alert('Enter valid city name!')}
 btn.addEventListener('click',()=>{
       let val=fil.value;
       todos(val).then((data)=>{
-        w.textContent=(data.days[0].icon).toUpperCase();
-        t.textContent=data.days[0].temp;
+        w.textContent=(data.days[0].hours[0].icon).toUpperCase();
+        t.textContent=data.days[0].hours[0].temp;
         p.textContent=(data.address).toUpperCase();
         fil.value='';
       }).catch((err)=>{
@@ -17,7 +17,8 @@ btn.addEventListener('click',()=>{
       })
 })
 async function todos(city) {
-      const response=await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=H522AE9PB5LXNEDUZTACU9KCM&contentType=json`);
+      const response=await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=H522AE9PB5LXNEDUZTACU9KCM&contentType=json
+      `);
       const data= await response.json();
       return data;
 
